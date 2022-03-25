75ebbaedeee3057d7604a987c39af097ae993200
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-25 18:08:50 +0300

    Add gallery app to the project settings

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # Created Apps
+    'gallery.apps.GalleryConfig'
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
