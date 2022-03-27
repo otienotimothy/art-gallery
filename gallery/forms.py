297412ec5017a7fb1a51b63e9cdf42a1aac397ee
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-27 15:37:56 +0300

    Add a LoginUserForm class to create a login form

--- gallery/forms.py
+++ gallery/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-from django.forms import EmailInput, TextInput
+from django.forms import EmailInput, PasswordInput, TextInput
 
 
 class RegisterUserForm(UserCreationForm):
@@ -21,4 +21,15 @@ class RegisterUserForm(UserCreationForm):
         self.fields['password2'].widget.attrs['class'] = 'form-control'
 
 
+class LoginUserForm(forms.Form):
+    username = forms.CharField(widget=TextInput(attrs={'class': 'form-control'}), max_length=100)
+    password = forms.CharField(widget=PasswordInput())
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+
+#     def __init__(self, *args, **kwargs):
+#         super(LoginUserForm, self).__init__(*args, **kwargs)
+
+#         self.fields['password'].widget.attrs['class'] = 'form-control'
