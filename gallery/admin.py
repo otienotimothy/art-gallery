bc33e6941c87939650bc96d8c86da7716c091ec7
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-28 18:10:10 +0300

    Add application models to the default admin site

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Photo, Location, Category
 
 # Register your models here.
+admin.site.register(Photo)
+admin.site.register(Location)
+admin.site.register(Category)
