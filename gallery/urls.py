77a417ad51e4840297bf3a21c723d8a8cd3b077d
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-28 19:20:39 +0300

    Modify the profile route to change name to profile from upload

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('signup/', signupUser, name= 'signup' ),
     path('login/', loginUser, name='login'),
-    path('profile/', upload, name='upload' )
+    path('profile/', upload, name='profile' )
 ]
