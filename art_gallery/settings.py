61e4ed6a862754a2733a7fb0e796cec95d1bb508
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-29 10:59:56 +0300

    Fix deployment error

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = 'django-insecure-ow)m98$8twptlq^3dtv$-_k!at&-6dmtc325ea24b0651!wce-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'art-lux.herokuapp.com'
+]
 
 
 # Application definition
